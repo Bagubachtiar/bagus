3fef41bc2c36864569680c5611f1692ad27088b4
bagusbachtiar <bagusbachtiarriski@gmail.com>
2021-06-25 13:06:01 +0700

    JsonDBMovie

---
+++ JsonMovieDB/app/src/main/java/com/bagusbachtiar/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,14 @@
+package com.bagusbachtiar.jsonmoviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=a18686e015158d29c97ac762920b061c";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
+
